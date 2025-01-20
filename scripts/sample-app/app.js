742a19a06ef850dda74595572d9d0dfb50e303ae
vlad0sn <vladsenechal@gmail.com>
2025-01-20 16:16:06 +0100

    Introduce intentional error

--- scripts/sample-app/app.js
+++ scripts/sample-app/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('DevOps Labs!');
 });
 
 app.get('/name/:name', (req, res) => {
